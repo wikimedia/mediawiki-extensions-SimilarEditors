5f58d35097e75b75791b8b50db9236eea4445bba
Thalia <thalia.e.chan@googlemail.com>
2022-05-11 22:45:48 +0100

    Return data from MockSimilarEditorsClient::getSimilarEditors
    Bug: T297684
    Change-Id: I57c26d02e2351039556d1768201d1ab0234118da

--- src/MockSimilarEditorsClient.php
+++ src/MockSimilarEditorsClient.php
@@ -15,6 +15,27 @@ class MockSimilarEditorsClient implements Client {
 	 * @inheritDoc
 	 */
 	public function getSimilarEditors( string $editor ) {
-		return [];
+		return [
+			new Neighbor(
+				'SomeUser1',
+				100,
+				10,
+				0.2,
+				0.4,
+				new TimeOverlap( 0.6, 'medium' ),
+				new TimeOverlap( 0.8, 'high' ),
+				null
+			),
+			new Neighbor(
+				'SomeUser2',
+				500,
+				5,
+				0.7,
+				0.5,
+				new TimeOverlap( 0.3, 'low' ),
+				new TimeOverlap( 0.1, 'low' ),
+				null
+			),
+		];
 	}
 }
