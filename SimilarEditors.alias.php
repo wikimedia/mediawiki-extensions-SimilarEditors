aaf5d1b1b7cd29b7a75a29ead9f25362d9b3ab6d
Jon Harald Søby <jhsoby@gmail.com>
2023-06-09 10:51:08 +0200

    Add Norwegian special page aliases
    Add translations of Special:SimilarEditors for Norwegian Bokmål (nb)
    and Norwegian Nynorsk (nn).
    
    Change-Id: I1d959fa62a6aec7212a8b02d73d35a48952afcd2

--- SimilarEditors.alias.php
+++ SimilarEditors.alias.php
@@ -12,3 +12,13 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'SimilarEditors' => [ 'SimilarEditors' ],
 ];
+
+/** Norwegian Bokmål (Norsk bokmål) */
+$specialPageAliases['nb'] = [
+	'SimilarEditors' => [ 'Lignende_bidragsytere', 'Liknende_bidragsytere' ],
+];
+
+/** Norwegian Nynorsk (Norsk nynorsk) */
+$specialPageAliases['nn'] = [
+	'SimilarEditors' => [ 'Liknande_bidragsytarar' ],
+];
