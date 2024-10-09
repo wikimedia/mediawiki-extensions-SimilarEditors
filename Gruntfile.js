c38b3074b91388a54c5b4d6cc717a3789aabe673
Umherirrender <umherirrender_de.wp@web.de>
2024-10-09 23:32:36 +0200

    build: Load banana message dirs from extension.json
    Common pattern to share the folder list with extension.json
    Remove allowTrailingWhitespace, no longer needed (9f1b8c3433)
    
    Change-Id: Ib7d9d41756e9f2603655544f426f834c56f0bd78

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function ( grunt ) {
+	const conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
@@ -20,14 +21,7 @@ module.exports = function ( grunt ) {
 				'!vendor/**'
 			]
 		},
-		banana: {
-			all: {
-				options: { allowTrailingWhitespace: true },
-				src: [
-					'i18n/', 'i18n/api/'
-				]
-			}
-		}
+		banana: conf.MessagesDirs
 	} );
 
 	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
